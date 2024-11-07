b031cd3946327cf0bb82537a946bdda6d2535420
Dectre <amirali.dh84@gmail.com>
2024-11-07 17:11:52 +0330

    Added threat function

--- Q1-810102443.cpp
+++ Q1-810102443.cpp
@@ -2,20 +2,30 @@
 #include <vector>
 using namespace std;
 
+typedef vector<vector<char> > Board;
+
 const int SIZE = 8;
 
+bool threat(Board board, int row, int col) {
+    for (int i = 0; i < SIZE; i++) {
+        if (board[i][col] == 'Q')
+            return true;
+    }
+    for (int j = 0; j < SIZE; j++) {
+        if (board[row][j] == 'Q')
+            return true;
+    }
+    for (int i = row, j = col; 0 <= i && j < SIZE; i--, j++) {
+        if (board[i][j] == 'Q')
+            return false;
+    }
+    return true;
+}
 int main() {
-    vector<vector<char> > board(SIZE, vector<char>(SIZE));
-    for (int i = 0 ; i < SIZE ; i++) {
-        for (int j = 0 ; j < SIZE ; j++) {
+    Board board(SIZE, vector<char>(SIZE));
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
             cin >> board[i][j];
         }
-    }
-    cout << "Output: " << endl;
-    for (int i = 0 ; i < SIZE ; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            cout << board[i][j];
-        }
-        cout << endl;
     }
 }
