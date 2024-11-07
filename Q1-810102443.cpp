8ed939b1519e71e2edfa352fc7d7db3278100000
Dectre <amirali.dh84@gmail.com>
2024-11-07 16:45:31 +0330

    getting input and checking it

--- Q1-810102443.cpp
+++ Q1-810102443.cpp
@@ -5,13 +5,13 @@ using namespace std;
 const int SIZE = 8;
 
 int main() {
-    vector<vector<char> > board;
+    vector<vector<char> > board(SIZE, vector<char>(SIZE));
     for (int i = 0 ; i < SIZE ; i++) {
         for (int j = 0 ; j < SIZE ; j++) {
             cin >> board[i][j];
         }
     }
-    cout << endl;
+    cout << "Output: " << endl;
     for (int i = 0 ; i < SIZE ; i++) {
         for (int j = 0; j < SIZE; j++) {
             cout << board[i][j];
