89f0415c2a675011d96dbecfac2e8808fac61bb4
Dectre <amirali.dh84@gmail.com>
2024-11-07 23:06:10 +0330

    Added input and input checked

---
+++ Q2-810102443.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    string equation;
+    cin >> n;
+    cin >> equation;
+    cout << n << " " << equation << endl;
+    return 1;
+}
