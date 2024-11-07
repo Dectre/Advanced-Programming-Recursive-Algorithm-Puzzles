18359ac3d6ea57ec682f81e0d6438fd20b2980e9
Dectre <amirali.dh84@gmail.com>
2024-11-07 17:45:40 +0330

    Added Solve function

--- Q1-810102443.cpp
+++ Q1-810102443.cpp
@@ -6,26 +6,47 @@ typedef vector<vector<char> > Board;
 
 const int SIZE = 8;
 
-bool threat(Board board, int row, int col) {
-    for (int i = 0; i < SIZE; i++) {
-        if (board[i][col] == 'Q')
-            return true;
+bool threat(Board& board, int row, int col) {
+    for (int i = 0; i < row; i++) {
+        if (board[i][col] == 'Q') return true;
+    }
+
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+        if (board[i][j] == 'Q') return true;
+    }
+
+    for (int i = row, j = col; i >= 0 && j < SIZE; i--, j++) {
+        if (board[i][j] == 'Q') return true;
+    }
+
+    return false;
+}
+
+void solve(Board& board, int row, int& counter) {
+    if (row == SIZE) {
+        counter++;
+        return;
     }
     for (int j = 0; j < SIZE; j++) {
-        if (board[row][j] == 'Q')
-            return true;
+        if (board[row][j] == '*' || threat(board, row, j))
+            continue;
+        board[row][j] = 'Q';
+        solve(board, row + 1, counter);
+        board[row][j] = '.';
     }
-    for (int i = row, j = col; 0 <= i && j < SIZE; i--, j++) {
-        if (board[i][j] == 'Q')
-            return false;
-    }
-    return true;
 }
+
 int main() {
     Board board(SIZE, vector<char>(SIZE));
+    int counter = 0;
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             cin >> board[i][j];
         }
     }
+
+    solve(board, 0, counter);
+    cout << counter << endl;
+    return 0;
+
 }
